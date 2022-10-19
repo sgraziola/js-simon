b4329d24fd030ad5c480f58fc3ccd93ac6cf2add
sgraziola <simo.fdl@gmail.com>
2022-10-19 19:06:05 +0200

    Ex SimonSays : Add includes in functions confronta

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,7 +78,9 @@ function confrontaArrays (array1, array2){
     for (let i = 0; i < array1.length; i++) {
         for (let j = 0; j < array2.length; j++) {
             if(array1[i] == array2[j]) {
-                array3.push(array1[i]);
+                if (!array3.includes(array1[i])) {
+                    array3.push(array1[i]);
+                } 
             }
         }
     }
